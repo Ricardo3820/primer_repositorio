6474aa5b91fab76ded6ac7b6f9e934f79739d846
Ricardo3820 <rickyxbatman@gmail.com>
2026-06-17 18:16:29 -0500

    Add basic user input and age calculation

---
+++ qwerftgb .py
@@ -0,0 +1,6 @@
+nombre = input("¿Cuál es tu nombre?")
+edad = input("¿Cuántos años tienes?")
+
+edad_el_proximo_ano = edad + 1
+
+print("Hola, ", nombre, "¡en un año tendrás ", edad_el_proximo_ano, "años!")
